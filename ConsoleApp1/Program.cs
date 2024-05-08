6cee504a81a93bc2f3f3abb34bda3e9c2c2458e7
alirezarize <khosravinezhad.79@gmail.com>
2024-05-08 08:17:38 +0330

    New changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,10 @@
-﻿
-// Alireza Khosravi
+﻿// Alireza Khosravi
 // Mohsen Mohammadi
 void Sum(int a, int b) => Console.WriteLine(a+b);
-Sum(5, 6);
+
+
+int a = 0, b = 0;
+int.TryParse(Console.ReadLine(), out a);
+int.TryParse(Console.ReadLine(), out b);
+
+Sum(a, b);
