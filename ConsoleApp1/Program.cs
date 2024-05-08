52e8f89b89be6c49b365fdc7c27af616929fccde
alirezarize <khosravinezhad.79@gmail.com>
2024-05-08 10:14:17 +0330

    New version

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,8 +3,6 @@
 
 using System.Diagnostics;
 
-void Sum(int a, int b) => Console.WriteLine(a+b);
-
 String ConvertBack(byte[] inp) => new String(inp.Select(c => (char)Convert.ToChar(c)).ToArray());
 byte[] ConvertToByte(String inp) => inp.Select(c=>(byte)c).ToArray();
 
@@ -12,12 +10,35 @@ int a = 0, b = 0;
 int.TryParse(Console.ReadLine(), out a);
 int.TryParse(Console.ReadLine(), out b);
 
-Sum(a, b);
+MethodInvoker(new List<Func<int, int, int>>()
+{
+    new Func<int, int, int>((int a, int b) =>
+    {
+        return a * b;
+    }),
+    new Func<int, int, int>((int a, int b) =>
+    {
+        return a / b;
+    }),
+    new Func<int, int, int>((int a, int b) =>
+    {
+        return a + b;
+    }),
+    new Func<int, int, int>((int a, int b) =>
+    {
+        return a - b;
+    }),
+}, a, b);
+
+void MethodInvoker(List<Func<int, int, int>> methods, int inp1, int inp2)
+{
+    foreach(var method in methods)
+        Console.WriteLine(method(inp1, inp2));
+}
 
 class Student
 {
     public int Id { get; private set; }
     public String Username { get; set; }
     public UInt32 PPP { get; set; }
-
 }
