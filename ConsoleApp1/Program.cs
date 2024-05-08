881c6e15fef7a1f115c64b6d38c2673f86c8ee08
alirezarize <khosravinezhad.79@gmail.com>
2024-05-08 09:06:58 +0330

    New Changes 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,27 +4,9 @@
 using System.Diagnostics;
 
 void Sum(int a, int b) => Console.WriteLine(a+b);
-String ConvertBack(byte[] inp)
-{
-    String result = "";
-    for (int i = 0; i < inp.Length; i++)
-    {
-        result += (char)inp[i];
-    }
 
-    return result;
-}
-
-byte[] ConvertToByte(String inp)
-{
-    List<byte> b = new List<byte>();
-    for(int i = 0; i < inp.Length; i++)
-    {
-        b.Add((byte)inp[i]);
-    }
-
-    return b.ToArray();
-}
+String ConvertBack(byte[] inp) => new String(inp.Select(c => (char)Convert.ToChar(c)).ToArray());
+byte[] ConvertToByte(String inp) => inp.Select(c=>(byte)c).ToArray();
 
 int a = 0, b = 0;
 int.TryParse(Console.ReadLine(), out a);
